4fd0d49cbeb0f04cf5d74d0e8f1888c73b26be0b
Ukang'a Dickson <ukanga@users.noreply.github.com>
2020-10-23 15:02:35 +0300

    Cleanup syntax

--- outputs.tf
+++ outputs.tf
@@ -10,65 +10,65 @@ output "availability_zone" {
 
 output "key_name" {
   description = "List of key names of instances"
-  value       = ["${aws_instance.ec2-instance.*.key_name}"]
+  value       = [aws_instance.ec2-instance.*.key_name]
 }
 
 output "public_dns" {
   description = "List of public DNS names assigned to the instances. For EC2-VPC, this is only available if you've enabled DNS hostnames for your VPC"
-  value       = ["${aws_instance.ec2-instance.*.public_dns}"]
+  value       = [aws_instance.ec2-instance.*.public_dns]
 }
 
 output "eip" {
   description = "List of elastic IP addresses assigned to the instances, if applicable"
-  value       = ["${aws_eip.instance_eip.*.public_ip}"]
+  value       = [aws_eip.instance_eip.*.public_ip]
 }
 
 output "public_ip" {
   description = "List of public IP addresses assigned to the instances, if applicable"
-  value       = ["${aws_instance.ec2-instance.*.public_ip}"]
+  value       = [aws_instance.ec2-instance.*.public_ip]
 }
 
 output "network_interface_id" {
   description = "List of IDs of the network interface of instances"
-  value       = ["${aws_instance.ec2-instance.*.network_interface}"]
+  value       = [aws_instance.ec2-instance.*.network_interface]
 }
 
 output "primary_network_interface_id" {
   description = "List of IDs of the primary network interface of instances"
-  value       = ["${aws_instance.ec2-instance.*.primary_network_interface_id}"]
+  value       = [aws_instance.ec2-instance.*.primary_network_interface_id]
 }
 
 output "private_dns" {
   description = "List of private DNS names assigned to the instances. Can only be used inside the Amazon EC2, and only available if you've enabled DNS hostnames for your VPC"
-  value       = ["${aws_instance.ec2-instance.*.private_dns}"]
+  value       = [aws_instance.ec2-instance.*.private_dns]
 }
 
 output "private_ip" {
   description = "List of private IP addresses assigned to the instances"
-  value       = ["${aws_instance.ec2-instance.*.private_ip}"]
+  value       = [aws_instance.ec2-instance.*.private_ip]
 }
 
 output "security_groups" {
   description = "List of associated security groups of instances"
-  value       = ["${aws_instance.ec2-instance.*.security_groups}"]
+  value       = [aws_instance.ec2-instance.*.security_groups]
 }
 
 output "vpc_security_group_ids" {
   description = "List of associated security groups of instances, if running in non-default VPC"
-  value       = ["${aws_instance.ec2-instance.*.vpc_security_group_ids}"]
+  value       = [aws_instance.ec2-instance.*.vpc_security_group_ids]
 }
 
 output "subnet_id" {
   description = "List of IDs of VPC subnets of instances"
-  value       = ["${aws_instance.ec2-instance.*.subnet_id}"]
+  value       = [aws_instance.ec2-instance.*.subnet_id]
 }
 
 output "credit_specification" {
   description = "List of credit specification of instances"
-  value       = ["${aws_instance.ec2-instance.*.credit_specification}"]
+  value       = [aws_instance.ec2-instance.*.credit_specification]
 }
 
 output "tags" {
   description = "List of tags of instances"
-  value       = ["${aws_instance.ec2-instance.*.tags}"]
+  value       = [aws_instance.ec2-instance.*.tags]
 }
